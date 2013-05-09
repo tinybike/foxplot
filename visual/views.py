a6b3ac367a9e5760a4128f71d39a8e36d293c12a
Jack Peterson <jack@tinybike.net>
2013-05-09 14:52:32 -0700

    plotter integration

--- visual/views.py
+++ visual/views.py
@@ -16,6 +16,7 @@ def results(request):
 	get_histogram = True if 'get_histogram' in request.POST.keys() else False
 	get_time_series = True if 'get_time_series' in request.POST.keys() else False
 	log_y = True if 'log_y' in request.POST.keys() else False
+	show_errors = True if 'show_errors' in request.POST.keys() else False
 
 	#if dataset == 'bio' or dataset == 'gro':
 	#	table = 'piedata'
@@ -33,36 +34,41 @@ def results(request):
 	#	P.summary['yearly'][table][dataset][2007].keys(), 
 	#	P.summary['yearly'][table][dataset][2007].values()
 	#)
-	histogram = zip(P.hist['bin'][dataset], P.hist['percent'][dataset])
+	histogram = zip(P.hist['bin']['bio'], P.hist['percent']['bio'])
 	
-	time_series = zip(
-		P.time_series[dataset]['year'],
-		P.time_series[dataset]['mean'],
-		P.time_series[dataset]['std']
-	)
+	#time_series = zip(
+	#	P.time_series[dataset]['year'],
+	#	P.time_series[dataset]['mean'],
+	#	P.time_series[dataset]['std']
+	#)
 	
 	json_time_series = {}
 	for field in ['bio', 'gro', 'bio_all', 'npp_wet', 'anpp']:
 		if log_y:
 			values = [None if j == 0 else log(j) for j in P.time_series[field]['mean']]
+			data = zip(P.time_series[field]['year'], values)
 		else:
 			values = P.time_series[field]['mean']
-		data = zip(
-			P.time_series[field]['year'], values
-		)
+			if show_errors:
+				error_bars = P.time_series[field]['std']
+				data = zip(P.time_series[field]['year'], values, error_bars)
+			else:
+				data = zip(P.time_series[field]['year'], values)
 		data = [list(j) for j in data]
 		json_time_series[field] = {'label': field, 'data': data}
 	
 	return render(request, 'visual/results.html', {
-		'dataset': dataset,
-		'table': table,
-		'get_summary': get_summary,
+		#'dataset': dataset,
+		#'table': table,
+		#'get_summary': get_summary,
 		'get_histogram': get_histogram,
 		'get_time_series': get_time_series,
+		'show_errors': show_errors,
+		'log_y': log_y,
 		#'summary': summary,
 		'num_bins': P.num_bins,
 		'histogram': histogram,
-		'time_series': time_series,
+		#'time_series': time_series,
 		'json_time_series': json.dumps(json_time_series),
 		'data': P.data,
 	})
